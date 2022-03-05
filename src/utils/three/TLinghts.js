77fac7669cd0eaab1ea4e0a09f373706ca19c49a
F999999999 <9811@88.com>
2022-03-05 15:15:51 +0800

    调整场景和灯光效果

--- src/utils/three/TLinghts.js
+++ src/utils/three/TLinghts.js
@@ -4,12 +4,11 @@ import { AmbientLight, PointLight, SpotLight } from "three";
 export const lightsList = [];
 
 // 环境光(颜色,强度,范围,衰减)
-const ambientLight = new AmbientLight(0xffffff, 1);
-
+const ambientLight = new AmbientLight(0xffffff, 1.2);
 // 点光源
-export const pointLight = new PointLight(0xffffff, 1, 5000, 0.1);
+export const pointLight = new PointLight(0xffffff, 1.6, 10000, 0.1);
 // 设置点光源位置
-pointLight.position.set(0, 3000, 0);
+pointLight.position.set(2000, 3000, -1000);
 
 // 聚光灯(颜色,强度,距离,散射角度,半影衰减,距离衰减)
 export const spotLight = new SpotLight(
@@ -21,7 +20,7 @@ export const spotLight = new SpotLight(
   0
 );
 // 设置聚光灯位置
-spotLight.position.set(-300, 1000, -300);
+spotLight.position.set(0, 5000, -3000);
 // 是否产生阴影
 spotLight.castShadow = true;
 
