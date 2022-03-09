b8c05a5fc3cb19d42866df87e69c58c65d9dcaef
F999999999 <9811@88.com>
2022-03-09 22:25:47 +0800

    组件库国际化配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="locale">
+    <router-view />
+  </a-config-provider>
 </template>
+
+<script>
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+
+export default {
+  data() {
+    return {
+      locale: zhCN,
+    };
+  },
+};
+</script>
 
 <style lang="less">
 #app {
