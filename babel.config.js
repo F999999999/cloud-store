110aa387388230fa226cccb25d058b55f5a7dcbe
F999999999 <9811@88.com>
2022-03-11 21:08:26 +0800

    build remove console.log

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  env: {
+    development: {
+      plugins: ["dynamic-import-node"],
+    },
+    production: {
+      plugins: ["transform-remove-console"], // remove console.log
+    },
+  },
 };
