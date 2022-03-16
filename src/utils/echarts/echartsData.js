9f65f470c6116139d40d0133f1586e5576ab08ff
F999999999 <9811@88.com>
2022-03-16 17:42:17 +0800

    Update

--- src/utils/echarts/echartsData.js
+++ src/utils/echarts/echartsData.js
@@ -287,7 +287,6 @@ function draw() {
 function getCirlPoint(x0, y0, r, angle) {
   let x1 = x0 + r * Math.cos((angle * Math.PI) / 180);
   let y1 = y0 + r * Math.sin((angle * Math.PI) / 180);
-  console.log(2);
   return {
     x: x1,
     y: y1,
@@ -296,5 +295,4 @@ function getCirlPoint(x0, y0, r, angle) {
 setInterval(function () {
   //用setInterval做动画感觉有问题
   draw();
-  console.log(1);
 }, 100);
